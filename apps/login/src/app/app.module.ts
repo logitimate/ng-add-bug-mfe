d27b9718278286d7cc8339fb8c46c74b21b6f96b
Logan Livingston <loganlivingston@fico.com>
2021-10-19 12:27:17 -0500

    [LL] skeleton for ng-add bug in mfes

--- apps/login/src/app/app.module.ts
+++ apps/login/src/app/app.module.ts
@@ -1,12 +1,16 @@
+import { RemoteEntryModule } from './remote-entry/entry.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgbModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
